6829bfa90f3ff9b7e0adeff60156359841840030
xuewa <366653992@qq.com>
2017-06-29 07:09:01 +0800

    add food

---
+++ src/components/food/food.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="food">
+		<!-- img -->
+		<div class="food_img">
+			<img src="">
+		</div>
+		<!-- title-baseInfo -->
+		<div class="food_baseInfo">
+			
+		</div>
+		<!-- food-desc -->
+		<div class="food_desc">
+			
+		</div>
+		<!-- food-rating -->
+		<div class="rating">
+			
+		</div>
+	</div>
+</template>
+
+<script type="text/ecmascript-6">
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
