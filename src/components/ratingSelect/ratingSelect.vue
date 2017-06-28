5202a8b89ade5933c8d138d1d3e43a83d87e588a
xuewa <366653992@qq.com>
2017-06-29 07:29:41 +0800

    madd ratingSelect

--- src/components/ratingSelect/ratingSelect.vue
+++ src/components/ratingSelect/ratingSelect.vue
@@ -1,12 +1,41 @@
 <template>
-  <div>I am ratings</div>
+  <div class="ratingSelect">
+  	<ul class="ratingTabs">
+  		<li class="allTab"></li>
+  		<li class="positiveTab"></li>
+  		<li class="negativeTab"></li>
+  	</ul>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  export default {
+    props: {
+      ratingType: {
+        type: Number,
+        default: 2
+      },
+      onlyContent: {
+        type: Boolean,
+        detault: true
+      },
+      ratingArr: {
+        type: Array,
+        default: []
+      }
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
 @import "../../common/stylus/mixin.styl";
-
+  .ratingTabs
+    li
+      color: rgb(77,85,93)
+      font-size: 12px
+      line-height: 16px
+      border-radius: 24px
+      &.active
+        color: #fff
+        background-color: rgb(0,160,220)
 </style>
