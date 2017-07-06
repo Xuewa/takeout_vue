2728e9ae555b67b54027092c58e909304d2edf93
xuewa <366653992@qq.com>
2017-07-06 22:29:01 +0800

    seller second commit

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,64 +1,71 @@
 <template>
   <div class="seller" ref="seller">
-  	<div class="seller-top">
-  		<div class="top-row">
-  			<span class="left-part">
-				<span class="inner-row">
-				  <font class="sellerName">{{seller.name}}</font>
-				</span>
-				<span class="inner-row">
-				  <star :score="seller.score" :size="36"></star><!--
-			   --><font class="ratingCount">({{seller.ratingCount}})</font><!--
-			   --><font class="sellCount">月售{{seller.sellCount}}单</font>
-				</span>
-  			</span>
-  			<span class="right-part">
-  		      	<i class="icon-favorite" :class="collectFlag? 'active' :''"></i><font class="collectTip">{{collectTip}}</font>
-  			</span>
-  		</div>
-  		<div class="bottom-row">
-  			<span class="minPrice">
-  				<span class="tip">起步价</span>
-  				<span class="data">
-  				  <font class="num">{{seller.minPrice}}</font><font class="unit">元</font></span>
-  			</span>
-  			<span class="deliveryPrice">
-  				<span class="tip">商家配送</span>
-  				<span class="data">
-  				  <font class="num">{{seller.deliveryPrice}}</font><font class="unit">元</font></span>
+    <div class="seller-container">
+    	<div class="seller-top">
+    		<div class="top-row">
+    			<span class="left-part">
+  				<span class="inner-row">
+  				  <font class="sellerName">{{seller.name}}</font>
   				</span>
-  			</span>
-  			<span class="deliveryTime">
-  				<span class="tip">平均配送时间</span>
-  				<span class="data">
-  			      <font class="num">{{seller.deliveryTime}}</font><font class="unit">分钟</font></span>
+  				<span class="inner-row">
+  				  <star :score="seller.score" :size="36"></star><!--
+  			   --><font class="ratingCount">({{seller.ratingCount}})</font><!--
+  			   --><font class="sellCount">月售{{seller.sellCount}}单</font>
   				</span>
-  			</span>
-  		</div>
-  	</div>
-  	<div class="seller-middle">
-  		<h2 class="seller-title">公共与活动</h2>
-      <div class="bulletin">
-        {{seller.bulletin}}
+    			</span>
+    			<span class="right-part" @click="toggleCollect($event)">
+    		      	<i class="icon-favorite" :class="collectFlag? 'active' :''"></i><font class="collectTip">{{collectTip}}</font>
+    			</span>
+    		</div>
+    		<div class="bottom-row">
+    			<span class="minPrice">
+    				<span class="tip">起步价</span>
+    				<span class="data">
+    				  <font class="num">{{seller.minPrice}}</font><font class="unit">元</font></span>
+    			</span>
+    			<span class="deliveryPrice">
+    				<span class="tip">商家配送</span>
+    				<span class="data">
+    				  <font class="num">{{seller.deliveryPrice}}</font><font class="unit">元</font></span>
+    				</span>
+    			</span>
+    			<span class="deliveryTime">
+    				<span class="tip">平均配送时间</span>
+    				<span class="data">
+    			      <font class="num">{{seller.deliveryTime}}</font><font class="unit">分钟</font></span>
+    				</span>
+    			</span>
+    		</div>
+    	</div>
+    	<div class="seller-middle">
+    		<h2 class="seller-title">公共与活动</h2>
+        <div class="bulletin">
+          {{seller.bulletin}}
+        </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item" v-for="item in seller.supports">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+    	</div>
+      <div class="seller-pics">
+        <h2 class="seller-title">商家实景</h2>
+        <div class="pics-wrapper" ref="picsWrapper">
+          <ul class="pics" ref="pics">
+            <li class="pic" v-for="pic in seller.pics" >
+              <img :src="pic" width="120" height="90" />
+            </li>
+          </ul>
+          
+        </div>
       </div>
-      <ul v-if="seller.supports" class="supports">
-        <li class="support-item" v-for="item in seller.supports">
-          <span class="icon" :class="classMap[item.type]"></span>
-          <span class="text">{{item.description}}</span>
-        </li>
-      </ul>
-  	</div>
-    <div class="seller-pics">
-      <h2 class="seller-title">商家实景</h2>
-      <div class="pics">
-        <img v-for="pic in seller.pics" :src="pic" />
+      <div class="seller-bottom">
+        <h2 class="seller-title">商家信息</h2>
+        <ul class="infos">
+          <li class="info" v-for="info in seller.infos">{{info}}</li>
+        </ul>
       </div>
-    </div>
-    <div class="seller-bottom">
-      <h2 class="seller-title">商家信息</h2>
-      <ul class="infos">
-        <li class="info" v-for="info in seller.infos">{{info}}</li>
-      </ul>
     </div>
   </div>
 </template>
@@ -76,7 +83,7 @@
     },
     data() {
       return {
-        collectFlag: true
+        collectFlag: false
       };
     },
     computed: {
@@ -91,14 +98,51 @@
     created() {
       this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special'];
     },
-    mouted() {
+    watch: {
+      'seller'() {
+        this.$nextTick(() => {
+          this._initSellerScroll();
+          this._initPicsScroll();
+        });
+      }
+    },
+    mounted() {
       this.$nextTick(() => {
+        this._initSellerScroll();
+        this._initPicsScroll();
+      });
+    },
+    methods: {
+      _initSellerScroll() {
         if (!this.sellerScroll) {
           this.sellerScroll = new BScroll(this.$refs.seller, {click: true});
         } else {
           this.sellerScroll.refrensh();
         }
-      });
+      },
+      _initPicsScroll() {
+        var width = 0;
+        if (this.seller.pics && this.seller.pics.length) {
+          let n = this.seller.pics.length;
+          const margin = 6;
+          const picWidth = 120;
+          width = (picWidth + margin) * n - margin;
+        }
+        this.$refs.pics.style.width = width + 'px';
+        this.$nextTick(() => {
+          if (!this.picsScroll) {
+            this.picsScroll = new BScroll(this.$refs.picsWrapper, {scrollX: true, eventPassthrough: 'vertical'});
+          } else {
+            this.picsScroll.refrensh();
+          }
+        });
+      },
+      toggleCollect(ev) {
+        if (!ev._constructed) {
+          return;
+        }
+        this.collectFlag = !this.collectFlag;
+      }
     },
     components: {
       star
@@ -110,6 +154,12 @@
 @import "../../common/stylus/mixin.styl";
   .seller
     background-color: #f3f5f7
+    position:absolute
+    top: 177px
+    bottom:0
+    left:0
+    right:0
+    overflow:hidden
     .seller-top
       border-1px(rgba(7, 17, 27, 0.1))
       background-color:#fff
@@ -145,6 +195,7 @@
           line-height: 24px
           width:50px
           text-align:center
+          color:rgba(77,85,93,.7)
           .active
             color: #f01414
           .collectTip
@@ -177,6 +228,8 @@
               font-size:10px
               color:rgb(7,17,27)
               font-weight:500
+        .deliveryTime
+          border-right:none
     .seller-middle
       border-top: 1px solid rgba(7,17,27,.1)
       border-1px(rgba(7,17,27,.1))
@@ -236,13 +289,20 @@
       padding:18px
       .seller-title
         margin-bottom:12px
-      .pics
-        img
-          width:120px
+      .pics-wrapper
+        overflow:hidden
+        .pics
           height:90px
-          margin-right:6px
-          &:last-child
-            margin-right:0
+          white-space: nowrap
+          .pic
+            display:inline-block
+            margin-right:6px
+            font-size:0
+            line-height:0
+            width:120px
+            height:90px
+            &:last-child
+              margin-right:0
     .seller-bottom
       margin-top:16px
       border-top: 1px solid rgba(7,17,27,.1)
