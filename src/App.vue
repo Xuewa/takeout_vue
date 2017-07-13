eacc2383ba1fd780c66b7cb6c06f72d9760cb10f
xuewa <366653992@qq.com>
2017-07-13 19:25:36 +0800

    mkeep-alive

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
         <router-link to="/seller">商家</router-link>
       </div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <router-view :seller="seller" keep-alive></router-view>
   </div>
 
 </template>
